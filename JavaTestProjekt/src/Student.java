c4976c68b65cfdb1871970d59d9c684a24054a02
meyer.lokal <meyer.lokal@172.18.128.43>
2019-01-16 17:26:15 +0100

    add project and Class Student

---
+++ JavaTestProjekt/src/Student.java
@@ -0,0 +1,21 @@
+
+public abstract class Student {
+	private String name;
+	private int gebJahr;
+	private String studiengang;
+	
+	
+	public Student(String name, int gebJahr, String studiengang) {
+		super();
+		this.name = name;
+		this.gebJahr = gebJahr;
+		this.studiengang = studiengang;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
