69b1bb9df7ed7d6ada3e2838f9358395dbad269a
meyer.lokal <meyer.lokal@172.18.128.43>
2019-01-16 17:42:35 +0100

    add method ausgeben

--- JavaTestProjekt/src/Student.java
+++ JavaTestProjekt/src/Student.java
@@ -11,11 +11,14 @@ public  class Student {
 		this.gebJahr = gebJahr;
 		this.studiengang = studiengang;
 	}
+	public void ausgeben() {
+		System.out.println(name + " / " + gebJahr +  " / " + studiengang);
+	}
 
 
 	public static void main(String[] args) {
 		Student s1 = new Student("Susi", 1997, "IMA");
-
+		s1.ausgeben();
 	}
 
 }
